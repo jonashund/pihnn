4a3b86a767d4c397a5721bf24bba2c210dddbb23
Jonas Hund <jonas.hund@mpe.au.dk>
2025-08-28 12:54:13 +0200

    work on script test_cracked_plate_phinn

--- scripts/test_cracked_plate_phinn.py
+++ scripts/test_cracked_plate_phinn.py
@@ -2,32 +2,25 @@
 Test from Section 4.2 in https://doi.org/10.1016/j.engfracmech.2025.111133
 """
 
-import sys
-
-# sys.path.append(r"C:\Windows\System32\site-packages\python3.10")
-sys.path.append(r"C:\Users\Nicolas\Desktop\Stage_2A\Deep Learning\pihnn-main")
-
-import pihnn.crack_detection as cd
-import os
 import torch
-import scipy
-import numpy as np
 import pihnn.nn as nn
 import pihnn.utils as utils
 import pihnn.geometries as geom
 import pihnn.graphics as graphics
 import pihnn.bc as bc
-import pihnn.crack_finding as cf
+import pihnn_devo.nn as nn_devo
+import pihnn_devo.utils as utils_devo
 
 # Network parameters
-n_epochs = 6000  # Number of epochs
-learn_rate = 1e-4  # Initial learning rate
-scheduler_apply = [1000, 2000, 3200, 4500, 5500]
-units = [1, 10, 10, 10, 1]  # Units in each network layer
-np_train = 300  # Number of training points on domain boundary
-np_test = 20  # Number of test points on the domain boundary
-beta = 0.5  # Initialization parameter
-gauss = 3  # Initialization parameter
+# n_epochs = 6000  # Number of epochs
+n_epochs = 10  # TODO: remove after debugging
+learn_rate = 1e-4  # initial learning rate
+scheduler_apply = [500, 1000, 1500]
+units = [1, 10, 10, 10, 1]  # units in each network layer
+np_train = 300  # number of training points on domain boundary
+np_test = 20  # number of test points on the domain boundary
+beta_param = 0.5  # initialization parameter
+gauss_param = 3  # initialization parameter
 
 # -----------------------------------
 # Geometry and boundary conditions
@@ -46,7 +39,7 @@ line3 = geom.line(P1=[-l, h], P2=[l, h], bc_type=bc.stress_bc(), bc_value=sig_ex
 line4 = geom.line(P1=[-l, h], P2=[-l, -h], bc_type=bc.stress_bc(), bc_value=0 + 0j)
 
 # ----------------------------------------------------------
-# horizontal crack in center of domain (-3 to 3)
+# horizontal crack in center of domain (-3,0) to (3,0)
 # ----------------------------------------------------------
 crack = geom.line(P1=-3 - 0j, P2=3 + 0j, bc_type=bc.stress_bc())
 
@@ -60,39 +53,46 @@ sig_xy_target = torch.tensor(
     [0.0538, 0.2477, -0.2496, -0.0526, -0.0532, -0.2485, 0.2490, 0.0531]
 )
 
-n_epochs = 2000  # Number of epochs
-learn_rate = 1e-5  # Initial learning rate
-scheduler_apply = [500, 1000, 1500]
-
 line1 = geom.line(P1=[-l, -h], P2=[l, -h], bc_type=bc.stress_bc(), bc_value=sig_ext_b)
 line2 = geom.line(P1=[l, -h], P2=[l, h], bc_type=bc.stress_bc(), bc_value=0 + 0j)
 line3 = geom.line(P1=[-l, h], P2=[l, h], bc_type=bc.stress_bc(), bc_value=sig_ext_t)
 line4 = geom.line(P1=[-l, h], P2=[-l, -h], bc_type=bc.stress_bc(), bc_value=0 + 0j)
 
 crack = geom.line(P1=-3 - 0j, P2=3 + 0j, bc_type=bc.stress_bc())
-crack.add_crack_tip(tip_side=0)  # Extrémité gauche
-crack.add_crack_tip(tip_side=1)  # Extrémité droite
+crack.add_crack_tip(tip_side=0)  # left crack tip
+crack.add_crack_tip(tip_side=1)  # Eright crack tip
 
 boundary = geom.boundary(
     [line1, line2, line3, line4, crack], np_train, np_test, enrichment="rice"
 )
 
-model = nn.enriched_PIHNN_finding("km", units, boundary)
-if __name__ == "__main__":
-    model.initialize_weights(
-        "exp", beta, boundary.extract_points(10 * np_train)[0], gauss
-    )
-    loss_train, loss_test, ListeZ1 = utils.train_finding(
-        sig_xx_target,
-        sig_yy_target,
-        sig_xy_target,
-        boundary,
-        model,
-        n_epochs,
-        learn_rate,
-        scheduler_apply,
-        scheduler_gamma=0.5,
-    )
-    graphics.plot_loss(loss_train, loss_test)
-    tria = graphics.get_triangulation(boundary)
-    graphics.plot_sol(tria, model, apply_crack_bounds=True)
+model = nn_devo.enriched_PIHNN_devo("km", units, boundary)
+
+model.initialize_weights(
+    method="exp",
+    beta=beta_param,
+    sample=boundary.extract_points(10 * np_train)[0],
+    gauss=gauss_param,
+)
+loss_train, loss_test, ListeZ1 = utils_devo.train_devo_adam(
+    sig_xx_target,
+    sig_yy_target,
+    sig_xy_target,
+    boundary,
+    model,
+    n_epochs,
+    learn_rate,
+    scheduler_apply,
+    scheduler_gamma=0.5,
+    dir="../test/test_cracked_plate_phinn/",
+)
+graphics.plot_loss(
+    loss_train=loss_train, loss_test=loss_test, dir="../test/test_cracked_plate_phinn/"
+)
+tria = graphics.get_triangulation(boundary)
+graphics.plot_sol(
+    triangulation=tria,
+    model=model,
+    apply_crack_bounds=True,
+    dir="../test/test_cracked_plate_phinn/",
+)
