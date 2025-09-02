3042f61778f600680dcaa4c5c2674ba3a27594b1
Jonas Hund <jonas.hund@mpe.au.dk>
2025-09-02 12:41:51 +0200

    use dictionary and file export to store network parameters

--- scripts/s03_test_cracked_plate_pihnn.py
+++ scripts/s03_test_cracked_plate_pihnn.py
@@ -15,16 +15,26 @@ os.environ["KMP_DUPLICATE_LIB_OK"] = (
     "True"  # set environment variable to avoid error message
 )
 
+out_dir = "../test/s03_test_cracked_plate_pihnn/"
+if not os.path.exists(out_dir):
+    os.makedirs(out_dir)
+
 # Network parameters
-n_epochs = 6000  # Number of epochs
-# n_epochs = 6  # Number of epochs
-learn_rate = 1e-5  # initial learning rate
-scheduler_apply = [500, 1000, 1500]
-units = [1, 10, 10, 10, 1]  # units in each network layer
-np_train = 300  # number of training points on domain boundary
-np_test = 20  # number of test points on the domain boundary
-beta_param = 0.5  # initialization parameter
-gauss_param = 3  # initialization parameter
+network_params = {
+    "n_epochs": 6000,
+    "learn_rate": 1e-5,
+    "scheduler_apply": [500, 1000, 1500],
+    "units": [1, 10, 10, 10, 1],
+    "np_train": 300,
+    "np_test": 20,
+    "beta": 0.5,
+    "gauss": 3,
+}
+utils_devo.export_network_params(params_dict=network_params, out_dir=out_dir)
+# utils_devo.import_network_params(
+#     filename="network_params.py", dir="../test/s01_collect_data_pihnn/"
+# )
+
 
 # -----------------------------------
 # Geometry and boundary conditions
@@ -34,7 +44,7 @@ l = 10  # 0.5*length of the domain
 n_segments = 50  # number of segments for each line
 
 # import stress data and coordinates from text files
-data_dir = "../test/collect_data_pihnn/"
+data_dir = "../test/s01_collect_data_pihnn/"
 stress_file = data_dir + "stress_data.txt"
 coords_file = data_dir + "coords_data.txt"
 stress_data = np.loadtxt(stress_file, skiprows=1)  # Skip header row
@@ -61,18 +71,20 @@ crack.add_crack_tip(tip_side=1)  # Right crack tip
 
 boundary = geom.boundary(
     curves=[line1, line2, line3, line4, crack],
-    np_train=np_train,
-    np_test=np_test,
+    np_train=network_params["np_train"],
+    np_test=network_params["np_test"],
     enrichment="rice",
 )
 
-model = nn_devo.enriched_pihnn_devo(PDE="km", units=units, boundary=boundary)
+model = nn_devo.enriched_pihnn_devo(
+    PDE="km", units=network_params["units"], boundary=boundary
+)
 
 model.initialize_weights(
     method="exp",
-    beta=beta_param,
-    sample=boundary.extract_points(10 * np_train)[0],
-    gauss=gauss_param,
+    beta=network_params["beta"],
+    sample=boundary.extract_points(10 * network_params["np_train"])[0],
+    gauss=network_params["gauss"],
 )
 loss_train, loss_test, _, _ = utils_devo.train_devo_adam(
     sig_xx_target=stress_data[:, 0],
@@ -82,11 +94,11 @@ loss_train, loss_test, _, _ = utils_devo.train_devo_adam(
     y_coords=coords_data[1],
     boundary=boundary,
     model=model,
-    n_epochs=n_epochs,
-    learn_rate=learn_rate,
-    scheduler_apply=scheduler_apply,
+    n_epochs=network_params["n_epochs"],
+    learn_rate=network_params["learn_rate"],
+    scheduler_apply=network_params["scheduler_apply"],
     scheduler_gamma=0.5,
-    dir="../test/test_cracked_plate_phinn/",
+    dir=out_dir,
 )
 
 tria = graphics.get_triangulation(boundary)
@@ -94,8 +106,10 @@ graphics.plot_sol(
     triangulation=tria,
     model=model,
     apply_crack_bounds=True,
-    dir="../test/test_cracked_plate_phinn/",
+    dir=out_dir,
 )
 graphics.plot_loss(
-    loss_train=loss_train, loss_test=loss_test, dir="../test/test_cracked_plate_phinn/"
+    loss_train=loss_train,
+    loss_test=loss_test,
+    dir=out_dir,
 )
